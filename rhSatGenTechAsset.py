92613d9af35cf4e7e7b5240c125cca562d3674e4
sumitgoel17 <sumit.goel@outlook.com>
2014-07-15 10:49:36 -0700

    Update rhSatGenTechAsset.py

--- rhSatGenTechAsset.py
+++ rhSatGenTechAsset.py
@@ -12,67 +12,77 @@ try:
   import getpass
   import time
   import csv
+  import re
 except ImportError, e:
   print 'ImportError:', e
   sys.exit(1)
 
-DFT_SRV = socket.gethostname()
+DEFAULT_SERVER = socket.gethostname()
 try:
-  SAT_SRV = raw_input("Enter RedHat Satellite Server Name [%s]: " \
-            % DFT_SRV)
+  SATELLITE_SERVER = raw_input("Enter RedHat Satellite Server Name [%s]: " % DEFAULT_SERVER)
 except KeyboardInterrupt:
   print ""
   sys.exit(1)
-if not SAT_SRV:
-    SAT_SRV = DFT_SRV
+if not SATELLITE_SERVER:
+    SATELLITE_SERVER = DEFAULT_SERVER
 
 try:
-  SAT_URL = "http://" + SAT_SRV + "/rpc/api"
-  CLT = xmlrpclib.Server(SAT_URL, verbose=0)
-  print "Welcome to RedHat Satellite Server %s" % CLT.api.systemVersion()
+  SATELLITE_URL = "http://" + SATELLITE_SERVER + "/rpc/api"
+  CLIENT = xmlrpclib.Server(SATELLITE_URL, verbose=0)
+  print "Welcome to RedHat Satellite Server %s" % CLIENT.api.systemVersion()
 except socket.error, e:
   print "Incorrect Server Name -", e
   print "Please run the script again."
   sys.exit(1)
 
-DFT_LIN = getpass.getuser()
+DEFAULT_LOGIN = getpass.getuser()
 try:
-  SAT_LIN = raw_input("Enter RedHat Satellite Login Name [%s]: " \
-            % DFT_LIN)
+  SATELLITE_LOGIN = raw_input("Enter RedHat Satellite Login Name [%s]: " % DEFAULT_LOGIN)
 except KeyboardInterrupt:
   print ""
   sys.exit(1)
-if not SAT_LIN:
-  SAT_LIN = DFT_LIN
+if not SATELLITE_LOGIN:
+  SATELLITE_LOGIN = DEFAULT_LOGIN
 
 try:
-  SAT_PWD = getpass.getpass("Enter Your Password: ")
+  SATELLITE_PASSWORD = getpass.getpass("Enter Your Password: ")
 except KeyboardInterrupt:
   print ""
   sys.exit(1)
-if not SAT_PWD:
+if not SATELLITE_PASSWORD:
   print "You did not enter anything. You need to run the script again!"
   sys.exit(1)
 
-CLT = xmlrpclib.Server(SAT_URL, verbose=0)
+CLIENT = xmlrpclib.Server(SATELLITE_URL, verbose=0)
 try:
-  KEY = CLT.auth.login(SAT_LIN, SAT_PWD)
-  ALLSYSTEMS = CLT.system.listSystems(KEY)
+  KEY = CLIENT.auth.login(SATELLITE_LOGIN, SATELLITE_PASSWORD)
+  ALLSYSTEMS = CLIENT.system.listSystems(KEY)
 except xmlrpclib.Fault, e:
   print 'xmlrpclib.Fault:', e
   sys.exit(1)
 
-FNAME = "rh_satellite_systems_dmi" + time.strftime("%Y%m%d%H%M%S") + ".csv"
-FPATH = "/tmp/" + FNAME
-OFILE = open(FPATH, 'wb')
-CSVFILE = csv.writer(OFILE,quoting=csv.QUOTE_ALL)
-CSVFILE.writerow(['Server Name','Vendor','Product','Asset'])
+FILENAME = "rh_satellite_systems_dmi_" + time.strftime("%Y%m%d%H%M%S") + ".csv"
+FILEPATH = "/Users/sumit.goel/Downloads/" + FILENAME
+OUTFILE = open(FILEPATH, 'wb')
+CSVFILE = csv.writer(OUTFILE,quoting=csv.QUOTE_ALL)
+CSVFILE.writerow(['Server Name','Vendor','Product','Asset','Serial'])
 
 for ITEM in reversed(ALLSYSTEMS):
-  SYSID = ITEM['id']
-  SYSNAME = ITEM['name']
+  SYSTEMID = ITEM['id']
+  SYSTEMNAME = ITEM['name']
   try:
-    SYSDMI = CLT.system.getDmi(KEY,SYSID)
+    SYSTEMDMI = CLIENT.system.getDmi(KEY,SYSTEMID)
+    SERIALSTRING = SYSTEMDMI['asset'].replace('.', '')
+    SERIALSTRING = SERIALSTRING.replace(' ', '')
+    SERIALNUMBER = re.search(r'(system):([\w-]+)', SERIALSTRING)
+    if SERIALNUMBER:
+      SERIALNUMBER = SERIALNUMBER.group(2)
+    else:
+      SERIALNUMBER = re.search(r'(board):(\w+)', SERIALSTRING)
+      if SERIALNUMBER:
+        SERIALNUMBER = SERIALNUMBER.group(2)
+      else:
+        break
   except socket.error, e:
     print "socket.error:", e
     break
@@ -83,11 +93,14 @@ for ITEM in reversed(ALLSYSTEMS):
     print "xmlrpclib.ProtocolError:", e
     break
   try:
-    CSVFILE.writerow([SYSNAME, SYSDMI['vendor'], SYSDMI['product'], \
-    SYSDMI['asset']])
+    CSVFILE.writerow([SYSTEMNAME, SYSTEMDMI['vendor'], SYSTEMDMI['product'], SYSTEMDMI['asset'], SERIALNUMBER])
   except csv.Error, e:
     print "Error:", e
     break
+
+OUTFILE.close()
+
+TECHASSETCSV = sys.argv[1] 
 
 CLT.auth.logout(KEY)
 #
